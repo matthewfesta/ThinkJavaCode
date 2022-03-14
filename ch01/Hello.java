0f2bc35ef04016a13c18f75100762b820a724af4
Matthew Festa <festa.matthew@me.com>
2022-03-14 08:59:01 -0400

    Exercises for chapter one.

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
+        System.out.println("How are you today?");
     }
-
+//this is a sample comment. 
 }
